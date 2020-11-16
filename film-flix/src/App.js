88bc397254cf37180ace98a8c6e9a4b212f36633
enwumeh <54593478+enwumeh@users.noreply.github.com>
2020-11-16 12:09:32 -0600

    toying aroung with unresolved renderring issue

--- film-flix/src/App.js
+++ film-flix/src/App.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React from 'react';
 import Layout from "./layout/Layout";
-import { Route, Switch } from 'react-router-dom';
+import "./App.css";
+
+import { Route, Switch } from "react-router-dom";
 import MainContainer from "./containers/MainContainer";
 
 function App() {
-
   return (
     <Layout>
       <Switch>
-      <Route path='/'>
-        <MainContainer />
+        <Route path='/'>
+          <MainContainer />
         </Route>
-    </Switch>
+      </Switch>
     </Layout>
   );
 }
