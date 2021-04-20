b20350a49e31e910b44ce5fa3ecbd1c3979e5763
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-04-20 13:20:01 -0500

    basic home page done

--- film-flix/src/screens/FimFlixHome/FilmFlixHome.jsx
+++ film-flix/src/screens/FimFlixHome/FilmFlixHome.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./FilmFlixHome.css"
 
 const FilmFlixHome = () => {
   return (
     <div className="flix-home">
-     This will say stuff 
+      <div className="enter-div">
+        <p className='enter-paragraph'>
+        Find a movie to watch tonight, or your new favorite show 
+        </p>
+        <button className='enter-button'>ENTER SITE</button>
+      </div>
     </div>
   )
 }
