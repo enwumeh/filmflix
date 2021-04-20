e59ef13410e86ab4ce4039033ca455e98f8d9221
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-04-20 13:36:38 -0500

    homepage link working

--- film-flix/src/screens/FimFlixHome/FilmFlixHome.jsx
+++ film-flix/src/screens/FimFlixHome/FilmFlixHome.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import "./FilmFlixHome.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./FilmFlixHome.css";
 
 const FilmFlixHome = () => {
   return (
     <div className="flix-home">
       <div className="enter-div">
-        <p className='enter-paragraph'>
-        Find a movie to watch tonight, or your new favorite show 
+        <p className="enter-paragraph">
+          Find a movie to watch tonight, or your new favorite show
         </p>
-        <button className='enter-button'>ENTER SITE</button>
+        <Link to="/movies"><button className=".enter-button">
+          ENTER SITE
+           </button>
+          </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default FilmFlixHome;
