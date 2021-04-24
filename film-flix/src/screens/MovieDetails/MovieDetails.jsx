c99c86c8278599d5092453f1b1d9da7188768680
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-04-24 15:14:21 -0500

    home page responsive for iphoneX

--- film-flix/src/screens/MovieDetails/MovieDetails.jsx
+++ film-flix/src/screens/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./MovieDetails.css";
 import { useParams } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 export default function MovieDetails(props) {
   const { deets } = props;
@@ -18,10 +18,7 @@ export default function MovieDetails(props) {
 
   const movieMatch = deets.find((movie) => movie.id == params.id);
   const picURL = `https://image.tmdb.org/t/p/w185/${movieMatch.poster_path}`;
-  const vidURL = `https://api.themoviedb.org/3//movie/${movieMatch}/videos?api_key=${apiKey}`
-  ;  
- 
-
+  const vidURL = `https://api.themoviedb.org/3/movie/${movieMatch.id}/videos?api_key=${apiKey}`;
   // useEffect(() => {
   //   const funct = (vidURL) => {
   //     let response = axios.get(vidURL);
@@ -31,27 +28,33 @@ export default function MovieDetails(props) {
   //   funct()
   // }, []);
 
-
-
   return deets ? (
     <div className="movie-home">
-      {console.log("movieMatch:", deets)}
+      {console.log("movieMatch:", movieMatch)}
 
-       {/* {console.log("dID:", dID)}  */}
-       <div className="details-text">
+      {/* {console.log("dID:", dID)}  */}
+      <div className="details-text">
         <p className="title"> {movieMatch.title} </p>
         <div className="release-date"> {movieMatch.release_date} </div>
         <p className="overview"> {movieMatch.overview} </p>
-      </div> 
+      </div>
       <div
         // key={movieMatch}
         className="image-dt"
         style={{
           backgroundImage: `url(${picURL})`,
         }}
-      ></div>
-      {/* {deets[13].overview}  */}
+      >
+      </div>
       <div className="page"></div>
+      <div
+        // key={movieMatch}
+        className="image-dt"
+        style={{
+          backgroundImage: `url(${vidURL})`,
+        }}
+      >
+      </div>
     </div>
   ) : //  (
   null;
